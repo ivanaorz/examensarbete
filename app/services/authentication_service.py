74b2dc1acf5cc429ac398840df4d052fb65383c3
ivanaorz <yhmu22.orziv@folkuniversitetet.nu>
2024-05-22 12:21:08 +0200

    Added authentication service for register and login

---
+++ app/services/authentication_service.py
@@ -0,0 +1,17 @@
+from models.user import User
+from utils.jwt_util import generate_token
+
+def register_user(username, email, password):
+    existing_user = User.find_by_email(email)
+    if existing_user:
+        return 'User already exists', False
+    new_user = User(username, email, password)
+    User.insert_user(new_user.__dict__)
+    return 'User registered successfully', True
+
+def login_user(email, password):
+    user_data = User.find_by_email(email)
+    if user_data and User(**user_data).check_password(password):
+        token = generate_token(user_data['_id'])
+        return token, True
+    return 'Invalid credentials', False
